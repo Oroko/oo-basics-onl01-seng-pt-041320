4c49ad3c28d3c1b1aeb846d79f5a841e89ee61d2
Abel Gechure <aoroko09@gmail.com>
2020-05-03 20:35:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 # Make your shoe class here!
 class Shoe 
-  attr_accessor :color, :size, :material
-  def initialize(brand,conditon)
+  attr_accessor :color, :size, :material, :condition
+  def initialize(brand)
     @brand = brand
-    @condition = "new"
+    
   end
   
   def brand
