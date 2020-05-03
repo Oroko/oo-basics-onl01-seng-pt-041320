4e76d9ebd9e0c3fdddb226226a04272771f397e8
Abel Gechure <aoroko09@gmail.com>
2020-05-03 20:14:30 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -12,8 +12,8 @@ class Shoe
   def cobble
     puts "Your shoe is as good as new!"
   end
-  def condition=(condition = "new")
-    @condition = condition
+  def condition=(condition)
+    @condition = "new"
   end
   def condition
     @condition
